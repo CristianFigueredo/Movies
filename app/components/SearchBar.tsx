ef4636ad75970ffdd1df628041f4a88f6d3af1cc
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-17 19:13:30 -0300

    feat(ui): SearchBar component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,37 @@
+import React, {FunctionComponent} from 'react';
+import {ViewStyle} from 'react-native';
+import {TextField, Card, Spacings, Colors} from 'react-native-ui-lib';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Spacer from './Spacer';
+
+type Props = {
+  onQueryChange?: (query: string) => void;
+};
+export const SearchBar: FunctionComponent<Props> = ({onQueryChange}) => {
+  return (
+    <Card containerStyle={$container}>
+      <SimpleLineIcons style={$icon} name="magnifier" size={20} color="black" />
+      <TextField
+        placeholderTextColor={Colors.$textNeutral}
+        placeholder="Search"
+        hideUnderline
+        onChangeText={onQueryChange}
+        containerStyle={$textField}
+      />
+    </Card>
+  );
+};
+
+const $container: ViewStyle = {
+  padding: Spacings.s4,
+  marginBottom: Spacings.s2,
+  flexDirection: 'row',
+};
+
+const $textField: ViewStyle = {
+  width: '100%',
+};
+
+const $icon: ViewStyle = {
+  marginRight: Spacings.s4,
+};
