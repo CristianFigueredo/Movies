0d017fcbde6ff56a88dc5ab346852e7154f98a33
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-17 19:16:50 -0300

    feat(ui): main screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {MovieCard} from './app/components/MovieCard';
+import {SearchBar} from './app/components/SearchBar';
+import Spacer from './app/components/Spacer';
+import {Colors, Spacings} from 'react-native-ui-lib';
+import {ViewStyle} from 'react-native';
 
 function App(): React.JSX.Element {
-  return;
-  <GestureHandlerRootView style={{flex: 1}}>
-    <SafeAreaView></SafeAreaView>;
-  </GestureHandlerRootView>;
+  return (
+    <GestureHandlerRootView style={$container}>
+      <Spacer height={Spacings.s5} />
+      <SearchBar />
+      <Spacer height={Spacings.s8} />
+      <MovieCard
+        posterURL="https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"
+        title="The Tomorrow War"
+        voteAverage={7.4}
+        overview="The world is stunned when a group of time travelers arrive from the year 2051 to deliver an urgent message: Thirty years in the future, mankind is losing a global war against a deadly alien species."
+        index={0}
+      />
+    </GestureHandlerRootView>
+  );
 }
+
+const $container: ViewStyle = {
+  flex: 1,
+  paddingHorizontal: 10,
+  backgroundColor: Colors.$backgroundNeutral,
+};
 
 export default App;
---
+++ app/screens/SearchMovie.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SearchMovie(): React.JSX.Element {
+  return <></>;
+}
