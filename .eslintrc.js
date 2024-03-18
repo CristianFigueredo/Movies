742d88cfb6b49421c7478489cecf285f0888b2c1
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-18 20:13:21 -0300

    feat: disable eslint rule 'react-hooks/exhaustive-deps'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  // want to disable the empty deps (exhaustive  deps) rule
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
