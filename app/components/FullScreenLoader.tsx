bb84c0b76548bd5368f24b1e03d34ed20b1a5138
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-18 17:12:53 -0300

    feat(ui): FullScreenLoader component

---
+++ app/components/FullScreenLoader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, ViewStyle} from 'react-native';
+import {Bounce} from 'react-native-animated-spinkit';
+import {Colors} from 'react-native-ui-lib';
+
+export const FullScreenLoader = () => {
+  return (
+    <View style={$fullScreenLoader}>
+      <Bounce size={33} color={Colors.primary} />
+    </View>
+  );
+};
+
+const $fullScreenLoader: ViewStyle = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
