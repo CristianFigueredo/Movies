4807f8d1336b9dabe99b5a0c2163c257d41d6de3
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-18 17:12:33 -0300

    feat(ui): EmptyStateFeedback component

--- app/screens/SearchMovie.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SearchMovie(): React.JSX.Element {
-  return <></>;
-}
---
+++ assets/index.ts
@@ -0,0 +1,7 @@
+export const IMAGES = {
+  /** Use contentFit="cover" to match your image size  */
+  GENERIC_IMAGE_PLACEHOLDER: require('./generic-image-placeholder.jpg'),
+  MOVIE_BACKDROP_PLACEHOLDER: require('./movie-backdrop-placeholder.jpg'),
+  TUMBLEWEED_IN_THE_DESERT: require('./tumbleweed-in-the-desert.png'),
+  NOT_FOUND: require('./not-found.png'),
+};
