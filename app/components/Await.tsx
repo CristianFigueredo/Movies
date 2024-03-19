0908f1d5cb4c23a3d3ee578805f654941926117b
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-19 12:13:15 -0300

    feat: Await component
    The Await component conditionally renders its children based on a boolean condition. It provides a fallback option to display while waiting for the condition to be met

---
+++ app/components/Await.tsx
@@ -0,0 +1,23 @@
+import React, {PropsWithChildren, useState, useEffect} from 'react';
+
+type AwaitProps = PropsWithChildren<{
+  for: boolean;
+  fallback: React.ReactNode;
+}>;
+
+/**
+ * A component that renders its children only when a condition is met.
+ */
+const Await: React.FC<AwaitProps> = ({for: condition, fallback, children}) => {
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    if (condition) {
+      setShouldRender(true);
+    }
+  }, [condition]);
+
+  return shouldRender ? <>{children}</> : <>{fallback}</>;
+};
+
+export default Await;
--- app/screens/Search.tsx
+++ app/screens/Search.tsx
@@ -6,12 +6,13 @@ import {Colors, Spacings, View} from 'react-native-ui-lib';
 import {Keyboard, ViewStyle} from 'react-native';
 import {BottomSheetModal} from '@gorhom/bottom-sheet';
 import useSearch from '../hooks/useSearch';
-import {MediaResponse} from '../services/omdb.types';
+import {MediaResponse, OmdbFilter} from '../services/omdb.types';
 import {FlashList} from '@shopify/flash-list';
 import {FullScreenLoader} from '../components/FullScreenLoader';
 import {EmptyStateFeedback} from '../components/EmptyStateFeedback';
 import {useNavigation} from '@react-navigation/native';
 import {FiltersBottomSheet} from '../components/FiltersBottomSheet';
+import Await from '../components/Await';
 
 export function SearchScreen(): React.JSX.Element {
   const search = useSearch();
@@ -23,6 +24,18 @@ export function SearchScreen(): React.JSX.Element {
     filtersModalRef.current?.present();
   }, []);
 
+  const handleFilterChange = useCallback(
+    (yearFilter: number, filter: OmdbFilter) => {
+      search.setYearFilter(yearFilter);
+      if (filter) {
+        search.setFilter(filter);
+      }
+      search.getNewResults(undefined, filter || undefined, yearFilter);
+      filtersModalRef.current?.dismiss();
+    },
+    [],
+  );
+
   return (
     <View style={$container}>
       <Spacer height={Spacings.s5} />
@@ -31,60 +44,45 @@ export function SearchScreen(): React.JSX.Element {
         onQueryChange={search.setQuery}
         onFilterPress={handlePresentModalPress}
       />
-      {search.loading && search.results.length === 0 ? (
-        <FullScreenLoader />
-      ) : (
-        <Fragment>
-          <Spacer height={Spacings.s3} />
-          <FlashList<MediaResponse>
-            data={search.results}
-            contentContainerStyle={$mediaList}
-            renderItem={({item, index}) => (
-              <MediaCard
-                posterURL={item.Poster}
-                title={item.Title}
-                rating={parseFloat(item.imdbRating)}
-                overview={item.Plot}
-                index={index}
-                onPress={() => {
-                  // TODO: fix types
-                  // @ts-ignore
-                  navigation.navigate('Details', {media: item});
-                }}
-              />
-            )}
-            estimatedItemSize={200}
-            onEndReachedThreshold={0.5}
-            onEndReached={search.getNextPage}
-            keyboardDismissMode="on-drag"
-            ListEmptyComponent={
-              <EmptyStateFeedback
-                imageID="not_found"
-                message="No results found"
-              />
-            }
-          />
-          <FiltersBottomSheet
-            ref={filtersModalRef}
-            yearFilter={search.yearFilter}
-            filter={search.filter}
-            onApplyPress={(yearFilterCandidate, filterCandidate) => {
-              let yearFilter: number | undefined = yearFilterCandidate;
-
-              search.setYearFilter(yearFilter);
-              if (filterCandidate) {
-                search.setFilter(filterCandidate);
-              }
-              search.getNewResults(
-                undefined,
-                filterCandidate || undefined,
-                yearFilter,
-              );
-              filtersModalRef.current?.dismiss();
-            }}
-          />
-        </Fragment>
-      )}
+      <Await
+        for={!search.loading && !(search.results.length === 0)}
+        fallback={<FullScreenLoader />}>
+        <Spacer height={Spacings.s3} />
+        <FlashList<MediaResponse>
+          data={search.results}
+          contentContainerStyle={$mediaList}
+          renderItem={({item, index}) => (
+            <MediaCard
+              posterURL={item.Poster}
+              title={item.Title}
+              rating={parseFloat(item.imdbRating)}
+              overview={item.Plot}
+              index={index}
+              onPress={() => {
+                // TODO: fix types
+                // @ts-ignore
+                navigation.navigate('Details', {media: item});
+              }}
+            />
+          )}
+          estimatedItemSize={200}
+          onEndReachedThreshold={0.5}
+          onEndReached={search.getNextPage}
+          keyboardDismissMode="on-drag"
+          ListEmptyComponent={
+            <EmptyStateFeedback
+              imageID="not_found"
+              message="No results found"
+            />
+          }
+        />
+        <FiltersBottomSheet
+          ref={filtersModalRef}
+          yearFilter={search.yearFilter}
+          filter={search.filter}
+          onApplyPress={handleFilterChange}
+        />
+      </Await>
     </View>
   );
 }
