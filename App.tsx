91a77ab717495f3d9e1af0249db4c14e90b56bbc
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-18 21:08:05 -0300

    build: add @react-navigation/native-stack

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
 import {setupReactNativeUILibraryTheme} from './app/theme/setup';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SearchScreen} from './app/screens/Search';
+import {NavigationContainer} from '@react-navigation/native';
+
 setupReactNativeUILibraryTheme();
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={$container}>
-      <BottomSheetModalProvider>
-        <SearchScreen />
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <GestureHandlerRootView style={$container}>
+        <BottomSheetModalProvider>
+          <SearchScreen />
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </NavigationContainer>
   );
 };
 
